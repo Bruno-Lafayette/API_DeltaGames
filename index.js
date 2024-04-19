066db68286ebb6f5d641bab4bbfde28780d691ac
Bruno Lafayette <brunolafayette@rocketmail.com>
2024-04-19 14:57:44 -0300

    atualizado index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const PORT = 5000;
-// const cors = require('cors')
+const cors = require('cors')
 const express = require("express");
-// const router = require('./src/routes/router')
+const router = require('./src/routes/router')
 const app = express();
 app.use(cors());
-// app.use(express.json());
-// app.use(router);
+app.use(express.json());
+app.use(router);
 
 
 app.use("/", (req, res)=>{
