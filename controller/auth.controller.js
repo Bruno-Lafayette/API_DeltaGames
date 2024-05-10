7af941da7ef0de12140fdf6761d599be5e618c0c
Bruno Lafayette <brunolafayette@rocketmail.com>
2024-05-10 09:56:21 -0300

    feat: add hash como resposta

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -45,9 +45,10 @@ const authController = {
         try {
             const { email, password } = req.body
             const [user, ] = await pool.query("select * from USUARIO where USUARIO_EMAIL = ?", [email])
-            if (!user[0]) return res.json({ error: "Invalid email!" })
+            if (!user[0]) return res.json({ message: "Usuario ou senha incorreto"  })
             
-            const { USUARIO_SENHA: hash, 
+            const { 
+                USUARIO_SENHA: hash, 
                 USUARIO_ID: id, 
                 USUARIO_NOME: name, 
                 USUARIO_EMAIL: userEmail, 
@@ -58,23 +59,15 @@ const authController = {
             if (check) {
                 const accessToken = jwt.sign({ userId: id }, '3812932sjad34&*@', { expiresIn: '1h' });
                 return res.json({ 
-                    // accessToken,
-                    // data: { 
-                    //     userId: id,
-                    //     name,
-                    //     email
-                    // }
-                    id, name, userEmail, cpf
+                    id, name, userEmail, cpf, hash
                  })
 
             }
-
-            return res.json({ error: "Wrong password!" })
-            
+            return res.json({ message: "Usuario ou senha incorreto" })
         } catch (error) {
             console.log(error)
             res.json({
-                error: error.message
+                message: error.message
             })
         }
     },
