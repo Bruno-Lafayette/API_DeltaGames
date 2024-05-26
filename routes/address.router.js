739ed15f0ba5182709c32a5f4db1bf153fbb1928
Bruno Lafayette <brunolafayette@rocketmail.com>
2024-05-25 22:08:33 -0300

    fix: correção rotas

--- routes/address.router.js
+++ routes/address.router.js
@@ -3,8 +3,8 @@ const router = express.Router()
 
 const addressController = require("../controller/address.controller")
 
-router.post("/address/add", addressController.addEnd)
-router.post("/address/edit", addressController.editEnd)
-router.post("/address/list", addressController.getAllEnds)
+router.post("/add", addressController.addEnd)
+router.post("/edit", addressController.editEnd)
+router.post("/list", addressController.getAllEnds)
 
 module.exports = router
