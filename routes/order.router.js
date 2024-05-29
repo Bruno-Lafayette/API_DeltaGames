f8e4eb9f511e4b4e9ffb0c13c8b41ffbac33d532
Bruno Lafayette <brunolafayette@rocketmail.com>
2024-05-29 16:35:01 -0300

    feat: criado metodo para criar pedido

--- routes/order.router.js
+++ routes/order.router.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const orderController = require("../controller/order.controller")
 
 router.post("/createOrder", orderController.create)
+router.post("/getAllOrders", orderController.getOrdersByUserId)
 
 module.exports = router
